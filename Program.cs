56404d0084129b5861fe04ae4725d076ea50cbb7
JorgeRZT <30592530+JorgeRZT@users.noreply.github.com>
2017-08-02 09:41:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caja caja = new Caja();
+            caja.maximoAlcanzado += Caja_maximoAlcanzado;
+        }
+
+        private static void Caja_maximoAlcanzado(object caja, CajaEventArgs evento)
+        {
+            throw new NotImplementedException();
+            //TODO:
+        }
+    }
+
+    public class Caja
+    {
+        public int Id { get; set; }
+        public decimal sumatoria = 0;
+        List<Ticket> tickets = new List<Ticket>();
+
+        public void addTicket(Ticket ticket)
+        {
+            sumatoria += ticket.valor;
+            tickets.Add(ticket);
+
+            if (sumatoria > 200)
+            {
+                CajaEventArgs args = new CajaEventArgs();
+                args.id = Id;
+                args.valor = sumatoria;
+                OnMaxMoney(args);
+            }
+        }
+
+        protected virtual void OnMaxMoney(CajaEventArgs evento)
+        {
+            CajaEventHandler handler = maximoAlcanzado;
+            if(handler != null)  handler(this, evento);
+        }
+
+        public event CajaEventHandler maximoAlcanzado;
+    }
+
+    public delegate void CajaEventHandler(Object caja, CajaEventArgs evento);
+
+    public class CajaEventArgs : EventArgs
+    {
+        public int id { get; set; }
+        public decimal valor { get; set; }
+    }
+
+    public class Ticket
+    {
+        public int id { get; set; }
+        public decimal valor { get; set; }
+    }
+}
